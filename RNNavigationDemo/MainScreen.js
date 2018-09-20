292b8c4050fb94e7c49c0f5f096989e68d81b0f7
kezhu <pengkezhu@7mtt.cn>
2018-09-20 21:23:25 +0800

    learn sheet alert

--- RNNavigationDemo/MainScreen.js
+++ RNNavigationDemo/MainScreen.js
@@ -1,54 +1,65 @@
-import {Platform, StyleSheet, Text, View, Image, ImageBackground, TouchableHighlight, ScrollView, Animated, Button} from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TouchableHighlight,
+  ScrollView,
+  Animated,
+  Button,
+  ActionSheetIOS,
+  Alert
+} from 'react-native';
 import React, {Component} from 'react';
 
 class FadeInView extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            fadeValue : new Animated.Value(0),
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      fadeValue: new Animated.Value(0)
     }
+  }
 
-    componentDidMount () {
-        Animated.timing (
-            this.state.fadeValue,
-            {
-                toValue: 1,
-                duration: 2000
-            }
-        ).start();
-    }
+  componentDidMount() {
+    Animated.timing(this.state.fadeValue, {
+      toValue: 1,
+      duration: 2000
+    }).start();
+  }
 
-    render () {
-        return (
-            <Animated.View style = {{...this.props.style, opacity: this.state.fadeValue}}>
-                {this.props.children}
-            </Animated.View>
-        );
-    }
+  render() {
+    return (<Animated.View style={{
+        ...this.props.style,
+        opacity: this.state.fadeValue
+      }}>
+      {this.props.children}
+    </Animated.View>);
+  }
 }
 
 // class FadeInView extends Component {
 //     constructor(props) {
 //         super(props);
 //         this.state = {
-//             fadeValue: new Animated.Value(0),          // 透明度初始值设为0
+//             fadeValue: new Animated.Value(0),           透明度初始值设为0
 //         };
 //     }
 //     componentDidMount() {
-//         Animated.timing(                            // 随时间变化而执行的动画类型
-//             this.state.fadeValue,                      // 动画中的变量值
+//         Animated.timing(                             随时间变化而执行的动画类型
+//             this.state.fadeValue,                       动画中的变量值
 //             {
-//                 toValue: 1,                             // 透明度最终变为1，即完全不透明
+//                 toValue: 1,                              透明度最终变为1，即完全不透明
 //             }
-//         ).start();                                  // 开始执行动画
+//         ).start();                                   开始执行动画
 //     }
 //     render() {
 //         return (
-//             <Animated.View                            // 可动画化的视图组件
+//             <Animated.View                             可动画化的视图组件
 //                 style={{
 //                     ...this.props.style,
-//                     opacity: this.state.fadeValue,          // 将透明度指定为动画变量值
+//                     opacity: this.state.fadeValue,           将透明度指定为动画变量值
 //                 }}
 //             >
 //                 {this.props.children}
@@ -57,81 +68,112 @@ class FadeInView extends Component {
 //     }
 // }
 
-
 class CC extends Component {
-    constructor (props) {
-        super(props)
-    }
-    render () {
-        return (
-            <View><Text>dddd</Text></View>
-        )
-    }
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (<View>
+      <Text>dddd</Text>
+    </View>)
+  }
 }
 
-export default class MainScreen extends  Component {
-    // static  navigationOptions = {
-    //   title : "DAO HANG LAN"
-    // };
-    static navigationOptions = {
-        title: '首页',
-    };
+export default class MainScreen extends Component {
+  // static  navigationOptions = {
+  //   title : "DAO HANG LAN"
+  // };
+  static navigationOptions = {
+    title: '首页'
+  };
 
-    _onPressButton(){
-        const { navigate } = this.props.navigation;
-        navigate('Profile', {name : 'Jane'})
-    }
+  _onPressButton() {
+    const {navigate} = this.props.navigation;
+    navigate('Profile', {name: 'Jane'})
+  }
 
-    _onPressMoviesButton() {
-        const { navigate } = this.props.navigation;
-        navigate('Movies')
-    }
+  _onPressMoviesButton() {
+    const {navigate} = this.props.navigation;
+    navigate('Movies')
+  }
 
-    _onPressLoginButton() {
-        const { navigate } = this.props.navigation;
-        navigate('Login')
-    }
+  _onPressLoginButton() {
+    const {navigate} = this.props.navigation;
+    navigate('Login')
+  }
 
-    _onPressSectionListButton () {
-        const { navigate } = this.props.navigation;
-        navigate('SectionList')
-    }
+  _onPressSectionListButton() {
+    const {navigate} = this.props.navigation;
+    navigate('SectionList')
+  }
 
-    _onPressSegmentControlButton () {
-        const { navigate } = this.props.navigation;
-        navigate('SegmentControl')
-    }
+  _onPressSegmentControlButton() {
+    const {navigate} = this.props.navigation;
+    navigate('SegmentControl')
+  }
 
-    render () {
-        return (
-            <FadeInView style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
-                <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Fading in</Text>
-                <Button title="点击跳转主页" onPress={this._onPressButton.bind(this)} />
-                <Button title="点击跳转电影页面" onPress={this._onPressMoviesButton.bind(this)} />
-                <Button title="点击跳转登录页面" onPress={this._onPressLoginButton.bind(this)} />
-                <Button title="点击跳转sectionList页面" onPress={this._onPressSectionListButton.bind(this)} />
-                <Button title="点击跳转SegmentControl页面" onPress={this._onPressSegmentControlButton.bind(this)} />
-            </FadeInView>
+  _showActionSheetIOS() {
+    ActionSheetIOS.showActionSheetWithOptions({
+      options: [
+        'cancel', 'confirm'
+      ],
+      cancelButtonIndex: 0,
+      title: '标题',
+      message: '提示'
+    }, (buttonIndex) => {
+      if (buttonIndex == 1) {
+        alert('I am sure');
+      }
+    })
+  }
 
-    );
-    }
+  _showAlert() {
+    Alert.alert('title', 'message', [
+      {
+        text: 'cancel',
+        onPress: () => alert('canceled')
+      }, {
+        text: 'confirm',
+        onPress: () => alert('confirmed')
+      }
+    ])
+  }
+
+  render() {
+    return (<FadeInView style={{
+        width: 250,
+        height: 50,
+        backgroundColor: 'powderblue'
+      }}>
+      <Text style={{
+          fontSize: 28,
+          textAlign: 'center',
+          margin: 10
+        }}>Fading in</Text>
+      <Button title="点击跳转主页" onPress={this._onPressButton.bind(this)}/>
+      <Button title="点击跳转电影页面" onPress={this._onPressMoviesButton.bind(this)}/>
+      <Button title="点击跳转登录页面" onPress={this._onPressLoginButton.bind(this)}/>
+      <Button title="点击跳转sectionList页面" onPress={this._onPressSectionListButton.bind(this)}/>
+      <Button title="点击跳转SegmentControl页面" onPress={this._onPressSegmentControlButton.bind(this)}/>
+      <Button title="点击展示actionSheet" onPress={this._showActionSheetIOS.bind(this)}/>
+      <Button title="点击展示alert" onPress={this._showAlert.bind(this)}/>
+    </FadeInView>);
+  }
+} {/* <ScrollView> */
+} {/* <Text style={styles.title}>Hello World</Text> */
+} {/*<Image source={{uri: 'https://qiniuuwmp3.7mtt.cn/VIPBanner.png', cache: 'only-if-cached'}}*/
+} {/*style={{width: 375, height: 100}} />*/
+} {/* <TouchableHighlight onPress = {this._onPressButton}> */
+} {/* <Text> Button </Text> */
+} {/* </TouchableHighlight> */
+} {/* </ScrollView> */
 }
-{/*<ScrollView>*/}
-    {/*<Text style={styles.title}>Hello World</Text>*/}
-    {/*<Image source={{uri: 'https://qiniuuwmp3.7mtt.cn/VIPBanner.png', cache: 'only-if-cached'}}*/}
-           {/*style={{width: 375, height: 100}} />*/}
-    {/*<TouchableHighlight onPress = {this._onPressButton}>*/}
-        {/*<Text> Button </Text>*/}
-    {/*</TouchableHighlight>*/}
-{/*</ScrollView>*/}
-
-
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  }
 });
